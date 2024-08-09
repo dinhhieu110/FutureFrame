bdcc2f22ea63d4e458a348413eacbc78fcab14f1
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-08-10 03:17:00 +0700

    [FilmFusion-3] Genres Dropdown Selection

---
+++ components/GenreDropdown.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Genres } from "../interfaces";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { ChevronDown } from "lucide-react";
+async function GenreDropdown() {
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 24, // 24h, even we have millions user, only 1 request to server for data --> caching
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="text-white flex justify-center items-center">
+        Genres <ChevronDown className="ml-1" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
+        {/* {data.genres.map(genre => (
+         
+            <DropdownMenuItem key ={genre.id} className="text-white">
+             <Link href={`/genre/${genre.id}?genre=${genre.name}` }>
+             {genre.name}
+             </Link>
+             </DropdownMenuItem>
+          
+        ))} */}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default GenreDropdown;
